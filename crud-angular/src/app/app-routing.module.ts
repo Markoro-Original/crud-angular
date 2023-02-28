5f18f4a11cec3f9ac5bada85cfa6ae8ab5b51b6b
Marcos Moraes <moraesvmarcos@gmail.com>
2023-02-28 14:38:58 -0300

    Aula 4

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CursosComponent} from "./cursos/cursos/cursos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'cursos', pathMatch: 'full'},
+  {path: '', component: CursosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
